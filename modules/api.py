c6d0dbb4e2792b214c7a4d89301f2bc77feaa026
Anderson Luiz <algobob@gmail.com>
2023-03-13 01:36:18 -0300

    keep max 1000 crash points in db

--- modules/api.py
+++ modules/api.py
@@ -2,11 +2,11 @@
 # Usage covered in <IDC lICENSE>
 # Jainam Oswal. <jainam.me> 
 from .crash_manager import calcular_probabilidades
-from .sqlite_helper import fetch_all_crash_points
+from .sqlite_helper import fetch_all_crash_points, deletar_velas_antigas
 
 # Import Libraries 
 from app import app
-from flask import jsonify
+from flask import jsonify, request
 
 # Define route "/api".
 @app.route('/api')
@@ -17,9 +17,16 @@ def api():
 
 @app.route('/api/blaze/crash/probabilidades')
 def probabilidades():
-  velas = fetch_all_crash_points()
+  max_velas = request.args.get("max_velas",default="1000")
+  velas = fetch_all_crash_points(max_velas)
+  app.logger.info("Quantidade de velas: ", len(max_velas))
+
   result = calcular_probabilidades(velas)
-  result["qtd_velas"] = len(velas)
+  qtd_velas = len(velas)
+  result["qtd_velas"] = qtd_velas
+  if qtd_velas > 1000:
+    deletar_velas_antigas(qtd_velas-1000)
+    
 
   # return in JSON format. (For API)
   return jsonify(result)  
--- modules/sqlite_helper.py
+++ modules/sqlite_helper.py
@@ -4,16 +4,15 @@ import psycopg2
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-def fetch_all_crash_points():
+def fetch_all_crash_points(max_velas):
     try:
-        sqliteConnection = psycopg2.connect(host='babar.db.elephantsql.com', database='zzdenalm', user='zzdenalm', password='ZArhVajSHYAd-Pux2dSdovDDaXCO1EZa')
-        # sqliteConnection = psycopg2.connect(host='localhost', database='postgres', user='postgres', password='mysecretpassword')
+        # sqliteConnection = psycopg2.connect(host='babar.db.elephantsql.com', database='zzdenalm', user='zzdenalm', password='ZArhVajSHYAd-Pux2dSdovDDaXCO1EZa')
+        sqliteConnection = psycopg2.connect(host='localhost', database='postgres', user='postgres', password='mysecretpassword')
         cursor = sqliteConnection.cursor()
         # print("Successfully Connected to SQLite")
-        sqlite_select_query = """SELECT * from crash_points"""
-        cursor.execute(sqlite_select_query)
+        sqlite_select_query = """SELECT * from crash_points ORDER BY created DESC LIMIT %s"""
+        cursor.execute(sqlite_select_query, max_velas)
         records = list(map(lambda row: row[0], cursor.fetchall()))
-        print("Quantidade de velas: ", len(records))
         cursor.close()
         return records
     except psycopg2.Error as error:
@@ -21,5 +20,20 @@ def fetch_all_crash_points():
     finally:
         if sqliteConnection:
             sqliteConnection.close()
-            # print("The SQLite connection is closed")            
-            
+            # print("The SQLite connection is closed")  
+
+def deletar_velas_antigas(qtd):
+    try:
+        # sqliteConnection = psycopg2.connect(host='babar.db.elephantsql.com', database='zzdenalm', user='zzdenalm', password='ZArhVajSHYAd-Pux2dSdovDDaXCO1EZa')
+        sqliteConnection = psycopg2.connect(host='localhost', database='postgres', user='postgres', password='mysecretpassword')
+        cursor = sqliteConnection.cursor()
+        sqlite_select_query = """DELETE FROM crash_points WHERE id IN (SELECT id FROM crash_points ORDER BY created ASC LIMIT %s);"""
+        cursor.execute(sqlite_select_query, qtd)
+        cursor.close()
+    except psycopg2.Error as error:
+        print("Failed to delete crashes points", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            # print("The SQLite connection is closed")  
+
