8b37ae79a0c84cb1f49470f67370090f4cc870f8
Anderson Luiz <algobob@gmail.com>
2023-03-16 02:01:15 -0300

    change host

--- modules/sqlite_helper.py
+++ modules/sqlite_helper.py
@@ -5,9 +5,12 @@ import psycopg2
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def fetch_all_crash_points():
+
+    sqliteConnection = None
+
     try:
-        # sqliteConnection = psycopg2.connect(host='babar.db.elephantsql.com', database='zzdenalm', user='zzdenalm', password='ZArhVajSHYAd-Pux2dSdovDDaXCO1EZa')
-        sqliteConnection = psycopg2.connect(host='localhost', database='postgres', user='aluiz', password='')
+        sqliteConnection = psycopg2.connect(host='babar.db.elephantsql.com', database='zzdenalm', user='zzdenalm', password='ZArhVajSHYAd-Pux2dSdovDDaXCO1EZa')
+        # sqliteConnection = psycopg2.connect(host='localhost', database='postgres', user='aluiz', password='')
         cursor = sqliteConnection.cursor()
         # print("Successfully Connected to SQLite")
         sqlite_select_query = """SELECT * from crash_points"""
@@ -23,9 +26,12 @@ def fetch_all_crash_points():
             # print("The SQLite connection is closed")  
 
 def deletar_velas_antigas(qtd):
+
+    sqliteConnection = None
+
     try:
-        # sqliteConnection = psycopg2.connect(host='babar.db.elephantsql.com', database='zzdenalm', user='zzdenalm', password='ZArhVajSHYAd-Pux2dSdovDDaXCO1EZa')
-        sqliteConnection = psycopg2.connect(host='localhost', database='postgres', user='aluiz', password='')
+        sqliteConnection = psycopg2.connect(host='babar.db.elephantsql.com', database='zzdenalm', user='zzdenalm', password='ZArhVajSHYAd-Pux2dSdovDDaXCO1EZa')
+        # sqliteConnection = psycopg2.connect(host='localhost', database='postgres', user='aluiz', password='')
         cursor = sqliteConnection.cursor()
         sqlite_select_query = "DELETE FROM crash_points WHERE id IN (SELECT id FROM crash_points ORDER BY created ASC LIMIT %s);"
         cursor.execute(sqlite_select_query, (qtd,))
