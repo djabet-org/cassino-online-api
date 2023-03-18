65aa78d36aa7a5548171de1c0053c290a9c08d48
Anderson Luiz <algobob@gmail.com>
2023-03-18 15:20:59 -0300

    add new pattern

--- modules/crash_manager.py
+++ modules/crash_manager.py
@@ -8,9 +8,10 @@ from __future__ import division
 def calcular_probabilidades(velas):
     
     return {
-        "Padrao (v,v,v,V) => G2: " : "%{0}".format(calculate_padrao1_g2(velas)*100),
-        "Padrao (v,v,V) => G2: " : "%{0}".format(calculate_padrao2_g2(velas)*100),
-        "Padrao (10v, V) => G2: " : "%{0}".format(calculate_padrao3_g2(velas)*100),
+        "Padrao (v,v,v) => G2: " : "%{0}".format(calculate_padrao1_g2(velas)*100),
+        "Padrao (v,v) => G2: " : "%{0}".format(calculate_padrao2_g2(velas)*100),
+        "Padrao (10v) => G2: " : "%{0}".format(calculate_padrao3_g2(velas)*100),
+        "Padrao (1.5p, 1.5p) => G2: " : "%{0}".format(calculate_padrao4_g2(velas)*100),
     }
 
 def calculate_padrao1(velas = []):
@@ -124,3 +125,16 @@ def calculate_padrao3_g2(velas = []):
             if velas[i+1] >= 2 or velas[i+2] >= 2 or velas[i+3] >= 2:
                 qtdSucesso += 1                   
     return float(qtdSucesso/qtdPadraoEncontrado)
+
+# Padrao (1.5p, 1.5p)
+def calculate_padrao4_g2(velas = []):
+    qtdSucesso = 0
+    qtdPadraoEncontrado = 0
+    for i in range(len(velas)-3):
+        if all(vela < 1.5 for vela in velas[i:i+2]):
+            print(velas[i:i+2])
+            qtdPadraoEncontrado += 1
+            if any( vela >= 2 for vela in velas[i+2:i+5]):
+                qtdSucesso += 1
+
+    return float(qtdSucesso/qtdPadraoEncontrado) if qtdPadraoEncontrado > 0 else 0
