042e3a880f6d2b2462cff74b9107bec916fad498
Anderson Luiz <algobob@gmail.com>
2023-09-27 15:07:35 -0300

    add white probability for each roll

--- modules/crash_manager.py
+++ modules/crash_manager.py
@@ -201,21 +201,21 @@ def calculate_rolls_distribution(rolls=[]):
 
 def calculate_roll_next_color_probability(rolls = []):
     result = dict({
-        0: [0,0,0],
-        1: [0,0,0],
-        2: [0,0,0],
-        3: [0,0,0],
-        4: [0,0,0],
-        5: [0,0,0],
-        6: [0,0,0],
-        7: [0,0,0],
-        8: [0,0,0],
-        9: [0,0,0],
-        10: [0,0,0],
-        11: [0,0,0],
-        12: [0,0,0],
-        13: [0,0,0],
-        14: [0,0,0],
+        0: [0,0,0,0],
+        1: [0,0,0,0],
+        2: [0,0,0,0],
+        3: [0,0,0,0],
+        4: [0,0,0,0],
+        5: [0,0,0,0],
+        6: [0,0,0,0],
+        7: [0,0,0,0],
+        8: [0,0,0,0],
+        9: [0,0,0,0],
+        10: [0,0,0,0],
+        11: [0,0,0,0],
+        12: [0,0,0,0],
+        13: [0,0,0,0],
+        14: [0,0,0,0],
       }
     )
 
@@ -226,21 +226,25 @@ def calculate_roll_next_color_probability(rolls = []):
         next_desired_rolls = rolls[i+1:i+4]
         anyRed = any( r["color"] == "red" for r in next_desired_rolls )
         anyBlack = any( r["color"] == "black" for r in next_desired_rolls )
+        anyWhite = any( r["color"] == "white" for r in next_desired_rolls )
 
         if anyRed:
             x[0] += 1
         if anyBlack:    
             x[1] += 1
+        if anyWhite:    
+            x[2] += 1    
 
-        x[2] += 1    
+        x[3] += 1    
 
         result[roll["roll"]] = x
 
     for key in result:
         value = result[key]
         result[key] = {
-            'red': 0 if value[2] == 0 else int((value[0]/value[2])*100),
-            'black': 0 if value[2] == 0 else int((value[1]/value[2])*100)
+            'red': 0 if value[3] == 0 else int((value[0]/value[3])*100),
+            'black': 0 if value[3] == 0 else int((value[1]/value[3])*100),
+            'white': 0 if value[3] == 0 else int((value[2]/value[3])*100)
         }    
     
     return result    
--- tests/test_numero_cor_probabilidade.py
+++ tests/test_numero_cor_probabilidade.py
@@ -43,6 +43,22 @@ class TestManager(unittest.TestCase):
 
         result = calculate_roll_next_color_probability(rolls)
         self.assertEqual(result[1]["red"], 50)
+
+    def test_numero_cor_probabilidade_white(self):
+        rolls = [
+            { 'roll': 1, 'color': 'red' },
+            { 'roll': 2, 'color': 'black' },
+            { 'roll': 2, 'color': 'white' },
+            { 'roll': 2, 'color': 'black' },
+            { 'roll': 2, 'color': 'red' },
+            { 'roll': 1, 'color': 'red' },
+            { 'roll': 2, 'color': 'white' },
+            { 'roll': 1, 'color': 'red' },
+            
+        ]
+
+        result = calculate_roll_next_color_probability(rolls)
+        self.assertEqual(result[1]["white"], 100)    
     
 if __name__ == '__main__':
     unittest.main()
